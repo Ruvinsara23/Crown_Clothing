69247ec6c6896ec254c6aceeafbfca1ad7c336b5
Pansilu Ruvinsara <101954975+Ruvinsara23@users.noreply.github.com>
2023-04-19 18:15:41 +0530

    added stripe

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,3 +1,4 @@
+
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -23,4 +24,4 @@ exports.handler = async (event) => {
       body: JSON.stringify({ error }),
     };
   }
-};
+}; 
--- src/component/payment-form/payment-form.jsx
+++ src/component/payment-form/payment-form.jsx
@@ -1,33 +1,57 @@
 import { CardElement ,useElements,useStripe} from "@stripe/react-stripe-js"
 import "./payment-form.style.scss"
 import Button from "../button/button-component";
+import { useSelector } from "react-redux";
+import { selectCurrentuser } from "../../store/user/use-selector";
+import { selectCartTotal } from "../../store/cart/cart-seletor";
+import { useState } from "react";
 
 
-
-
-
- const PaymentForm =()=>{
-    const stripe=useStripe();
-    const elements=useElements();
-    
+const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const amount = useSelector(selectCartTotal);
+    const currentUser = useSelector(selectCurrentuser);
+   
     const paymentHandler = async (e) => {
-        e.preventDefault();
-        if (!stripe || !elements) {
-          return;
-        }
-        //netlify\functions\create-payment-intent
-        const response = await fetch('/.netlify/functions/create-payment-intent', {
-          method: 'post',
-          headers: {
-            'Content-Type': 'application/json',
+      e.preventDefault();
+      if (!stripe || !elements) {
+        return;
+      }
+  
+      const response = await fetch('/.netlify/functions/create-payment-intent', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ amount: amount * 100 }),
+      }).then((res) => {
+        return res.json();
+      });
+  
+      const clientSecret = response.paymentIntent.client_secret;
+  
+      const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+          billing_details: {
+            name: currentUser ? currentUser.displayName : 'Abs',
           },
-          body: JSON.stringify({ amount: 1000}),
-        }).then(res =>res.json());
-        console.log(response)
-        };
-
-    return(
-       <div className='payment-form-container'>
+        },
+      });
+  
+  
+      if (paymentResult.error) {
+        alert(paymentResult.error.message);
+      } else {
+        if (paymentResult.paymentIntent.status === 'succeeded') {
+          alert('Payment Successful!');
+        }
+      }
+    };
+  
+    return (
+        <div className='payment-form-container'>
        <form className='form-container' onSubmit={paymentHandler}>
         <h2> 
         Cretdit Card Payment
@@ -36,8 +60,12 @@ import Button from "../button/button-component";
         <Button buttontype='inve' >Pay Now</Button>
         </form>
        </div>
-    ) 
+      
+    );
+  };
 
-    }
 
-export default PaymentForm;
+ export default PaymentForm;
+
+
+  
